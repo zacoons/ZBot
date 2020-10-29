05bd3c07869f1e1f8d0bbac066b9dc64842c87e2
Zacoons <73414084+Zacoons@users.noreply.github.com>
2020-10-30 07:46:35 +1000

    Create jokes.py

---
+++ jokes.py
@@ -0,0 +1,10 @@
+jokes = [
+        "I'm so good at sleeping. I can do it with my eyes closed.",
+        "I couldn't figure out why the baseball kept getting larger. Then it hit me.",
+        'A blind man walks into a bar. And a table. And a chair.',
+        "What did one hat say to the other? You stay here. I'll go on ahead.",
+        'One day there was a potato.',
+        'Hey! I invented a new word: plagiarism.',
+        "Did you hear about the mathematician who was afraid of negative numbers? He'd stop at nothing to avoid them",
+        "Why don't scientists trust atoms? They just make up everything."
+        ]
